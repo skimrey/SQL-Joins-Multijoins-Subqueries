c8e5b826e14cb2d31927e88d125fbe1a8852bf0b
skimrey <89326872+skimrey@users.noreply.github.com>
2023-03-02 23:50:02 -0600

    Update questions.sql

--- questions.sql
+++ questions.sql
@@ -1,4 +1,5 @@
 --1. List all customers who live in Texas (use JOINs)
+
 SELECT customer.first_name, customer.last_name, customer.email, district
 FROM customer
 FULL JOIN address
@@ -6,6 +7,7 @@ ON customer.address_id = address.address_id
 WHERE district = 'Texas';
 
 --2. Get all payments above $6.99 with the Customer's Full Name
+
 SELECT customer.first_name, customer.last_name, payment.amount
 FROM customer
 FULL JOIN payment
@@ -13,6 +15,7 @@ ON customer.customer_id = payment.customer_id
 WHERE amount > 6.99;
 
 --3. Show all customers names who have made payments over $175(use subqueries)
+
 SELECT distinct(customer_id), first_name, last_name 
 FROM customer
 WHERE customer_id IN(
@@ -24,6 +27,7 @@ WHERE customer_id IN(
 );
 
 --4. List all customers that live in Nepal (use the city table)
+
 SELECT customer.first_name, customer.last_name, country.country
 FROM customer
 FULL JOIN address
@@ -35,16 +39,20 @@ on city.country_id = country.country_id
 WHERE country.country = 'Nepal';
 
 --5. Which staff member had the most transactions?
+
 SELECT first_name, last_name, COUNT(payment.staff_id) as transactions
 FROM staff
 FULL JOIN payment
 ON payment.staff_id = staff.staff_id
 group by first_name, last_name
 order by COUNT(payment.staff_id) desc;
+
 --6. How many movies of each rating are there?
+
 select rating, count(title) from film f group by rating
 
 --7.Show all customers who have made a single payment above $6.99 (Use Subqueries)
+
 SELECT distinct(customer_id), first_name, last_name
 FROM customer
 WHERE customer_id IN(
@@ -54,8 +62,8 @@ WHERE customer_id IN(
 	HAVING amount > 6.99
 );
 
-
 --8. How many free rentals did our store give away?
+
 select count(rental.rental_id) - count(payment.payment_id) as rental_vs_payment_difference
 from rental
 FULL JOIN payment
